a0c9b771124845939ee5e7d194d017f809a3acc2
Firewallexaminer <113923745+Firewallexaminer@users.noreply.github.com>
2024-12-06 00:58:25 -0500

    Rep
    e

--- Logging Test.py
+++ Logging Test.py
@@ -1,7 +1,21 @@
 import psutil
+import pandas as pd
 import time
 
+
+
+# Creates a table with time, hostname, and CPU usage
+df = pd.DataFrame(columns=['Timestamp', 'CPU Usage', 'Hostname'])
+
+# While script is running, checks and prints the cpu usage every second
 while True:
     cpu_percent = psutil.cpu_percent(interval=1)
+    Timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
     print(f"CPU Usage: {cpu_percent}%")
     time.sleep(1)
+
+
+# 
+df = df.append({'Timestamp': timestamp, 'CPU Usage': cpu_percent, 'Hostname': "CamServer"}, ignore_index=True)
+df.to_csv('cpu_usage.csv', index=False)
+print(f'{timestamp} - CPU Usage: {cpu_usage}%')
