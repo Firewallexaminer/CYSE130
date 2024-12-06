a78bf08606e0e4491d14c4e9043636ebaaafa2a0
Firewallexaminer <113923745+Firewallexaminer@users.noreply.github.com>
2024-12-06 13:52:05 -0500

    Issue with reading cpu usage column

--- AnalysisAlgorithm.py
+++ AnalysisAlgorithm.py
@@ -1,2 +1,11 @@
 import pandas as pd
 
+
+url = 'https://raw.githubusercontent.com/Firewallexaminer/CYSE130/refs/heads/main/cpu_usage.csv'
+log = pd.read_csv(url)
+
+i = 0
+while i < 18:
+    if log['CPU Usage'] > 70.0:
+        print('Alert! Unusually High CPU Usage detected!')
+    i = i+1
