66fb92b7c3d532cd05e98fcbc1169b849f3e1f60
Firewallexaminer <113923745+Firewallexaminer@users.noreply.github.com>
2024-12-06 03:22:49 -0500

    LogSystemUpgrade
    Updated Logging System to include hostname and save everything to a csv file.

--- Logging Test.py
+++ Logging Test.py
@@ -1,19 +1,18 @@
-import os
 import psutil
 import pandas as pd
 import time
 
-print(os.getcwd())
-
 # Creates a table with time, hostname, and CPU usage
 df = pd.DataFrame(columns=['Timestamp', 'CPU Usage', 'Hostname'])
+
 
 # While script is running, checks and prints the cpu usage every second
 while True:
     cpu_percent = psutil.cpu_percent(interval=1)
     Timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
     print(f'{Timestamp} - CPU Usage: {cpu_percent}%')
+    new_row = pd.DataFrame([[Timestamp, cpu_percent, 'F3-C3-Switch']], columns=['Timestamp', 'CPU Usage', 'Hostname'])
+    df = pd.concat([df, new_row], ignore_index=True)
+    df.to_csv('cpu_usage.csv', index=False)
     time.sleep(1)
-
-df = df.append({'Timestamp': timestamp, 'CPU Usage': cpu_percent, 'Hostname': "CamServer"}, ignore_index=True)
-df.to_csv('cpu_usage.csv', index=False)
+    
