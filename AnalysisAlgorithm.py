817932d3a12ba6d5bc9550dbb8be1486da68b90c
Firewallexaminer <113923745+Firewallexaminer@users.noreply.github.com>
2024-12-06 14:58:59 -0500

    Added

--- AnalysisAlgorithm.py
+++ AnalysisAlgorithm.py
@@ -4,8 +4,12 @@ import pandas as pd
 url = 'https://raw.githubusercontent.com/Firewallexaminer/CYSE130/refs/heads/main/cpu_usage.csv'
 log = pd.read_csv(url)
 
-i = 0
-while i < 18:
-    if log['CPU Usage'] > 70.0:
-        print('Alert! Unusually High CPU Usage detected!')
-    i = i+1
+log.head()
+
+for value in log['CPU Usage']:
+    if value > 70.0:
+       warn = 'Syslog 4: Unusually High CPU Usage detected!'
+       log['Debug'] = warn
+       print(warn)
+        
+
